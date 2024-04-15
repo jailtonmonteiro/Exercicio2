f78521e460708ae68278428cece0d823019d2cb7
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-04-15 12:24:16 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+//Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+//casas decimais conforme exemplos.
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14159;
+            double raio;
+            double area;
+
+            Console.WriteLine("Digite o valor do raio do circulo: ");
+            raio = double.Parse(Console.ReadLine());
+
+            area = pi * (raio * raio);
+
+            Console.WriteLine(area.ToString("F4"));
+        }
+    }
+}
